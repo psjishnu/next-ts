80c98a4cb4d4c3bb8fd4318ea156d20f47840a10
psjishnu <psjishnu13@gmail.com>
2025-04-17 09:57:00 +0530

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Navbar from "../components/Navbar";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,6 +29,7 @@ export default function RootLayout({
         suppressHydrationWarning={true}
         className={`${geistSans.variable} ${geistMono.variable}`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
