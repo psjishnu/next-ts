936699c0da4317f34316fea9a3078cfee6d8b227
psjishnu <psjishnu13@gmail.com>
2025-04-17 12:20:49 +0530

    link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,24 +32,24 @@ function ResponsiveAppBar() {
           </Link>
 
           <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Typography>
+          <Link href="/">
+            <Typography
+              variant="h5"
+              noWrap
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              Home
+            </Typography>
+          </Link>
         </Toolbar>
       </Container>
     </AppBar>
